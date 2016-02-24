4021f30007e8d6fc3376d9ec65ca9fcd723c5696
vibhisharathod <vibhisha.saparia@gmail.com>
2016-02-24 13:01:49 +0530

    Agent Type Api

---
+++ Veritas.Web/API/AgentTypeMasterApiController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Veritas.DataAccess;
+using Veritas.Entities;
+
+namespace Veritas.Web.API
+{
+    public class AgentTypeMasterApiController : ApiController
+    {
+        // GET: CountryMasterApi
+        private readonly IAgentTypeMasterDA AgentTypeMasterDA;
+
+        public AgentTypeMasterApiController()
+        {
+
+        }
+
+        public AgentTypeMasterApiController(IAgentTypeMasterDA AgentTypeMaster)
+        {
+            AgentTypeMasterDA = AgentTypeMaster;
+        }
+
+        [HttpGet]
+        public string GiveMessage()
+        {
+            return "Me";
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Create(AgentTypeMaster prodData)
+        {
+            if (prodData != null)
+            {
+                await AgentTypeMasterDA.AddAgentTypeMaster(prodData);
+                return Ok();
+            }
+            return BadRequest("Request cannot be NULL");
+        }
+    }
+}
