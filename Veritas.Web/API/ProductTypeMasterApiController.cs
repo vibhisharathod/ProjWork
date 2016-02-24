8cd83292345abb4f20a95c2a683871371ede2883
vibhisharathod <vibhisha.saparia@gmail.com>
2016-02-24 17:15:27 +0530

    Untracked Added

---
+++ Veritas.Web/API/ProductTypeMasterApiController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Veritas.DataAccess;
+using Veritas.Entities;
+
+namespace Veritas.Web.API
+{
+    public class ProductTypeMasterApiController : ApiController
+    {
+        private readonly IGiProductTypeMasterDA giProductTypeMasterDA;
+
+        public ProductTypeMasterApiController()
+        {
+
+        }
+
+        public ProductTypeMasterApiController(IGiProductTypeMasterDA GiProductMaster)
+        {
+            giProductTypeMasterDA = GiProductMaster;
+        }
+
+        [HttpGet]
+        public string GiveMessage()
+        {
+            return "Me";
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Create(GiProductTypeMaster prodData)
+        {
+            if (prodData != null)
+            {
+                await giProductTypeMasterDA.AddProductTypeMaster(prodData);
+                return Ok();
+            }
+            return BadRequest("Request cannot be NULL");
+        }
+    }
+}
---
+++ Veritas.Web/API/StateMasterApiController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Veritas.DataAccess;
+using Veritas.Entities;
+
+
+namespace Veritas.Web.API
+{
+    public class StateMasterApiController : ApiController
+    {
+        private readonly IStateMasterDA StateMasterDA;
+
+        public StateMasterApiController()
+        {
+
+        }
+
+        public StateMasterApiController(IStateMasterDA StateMaster)
+        {
+            StateMasterDA = StateMaster;
+        }
+
+        [HttpGet]
+        public string GiveMessage()
+        {
+            return "Me";
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Create(StateMaster prodData)
+        {
+            if (prodData != null)
+            {
+                await StateMasterDA.AddStateMaster(prodData);
+                return Ok();
+            }
+            return BadRequest("Request cannot be NULL");
+        }
+    }
+}
